ecec6c204be181d199276a56ec01e87ceaad3961
Michael Bestas <mkbestas@lineageos.org>
2026-06-27 02:49:15 +0300

    voltage-priv: Add Android 17 keys
    Change-Id: I2968ac038f61c41522db6b4c0380feb31efe275d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- main/keys.mk
+++ main/keys.mk
@@ -67,6 +67,7 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.uwb.resources:com.android.uwb.resources.certificate.override \
     com.android.virt:com.android.virt.certificate.override \
     com.android.vndk.current:com.android.vndk.current.certificate.override \
+    com.android.webapp:com.android.webapp.certificate.override \
     com.android.wifi:com.android.wifi.certificate.override \
     com.android.wifi.dialog:com.android.wifi.dialog.certificate.override \
     com.android.wifi.resources:com.android.wifi.resources.certificate.override \
@@ -83,6 +84,9 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     ServiceConnectivityResources:com.android.connectivity.resources.certificate.override \
     ServiceUwbResources:com.android.uwb.resources.certificate.override \
     ServiceWifiResources:com.android.wifi.resources.certificate.override \
+    TelecomServiceResources:com.android.telecom.resources.certificate.override \
+    TelecomUi:com.android.telecomui.certificate.override \
+    WebAppService:com.android.webapp.certificate.override \
     WifiDialog:com.android.wifi.dialog.certificate.override \
     GmscompatLib:gmscompat_lib.certificate.override
 
